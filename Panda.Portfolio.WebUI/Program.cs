93705899ac0bc9e68b72d3972bb00eeaac0c1f08
p8b <majid.joveini@gmail.com>
2023-03-15 22:46:12 +0000

    Added DataProtection options to use certificate to protect keys

--- Panda.Portfolio.WebUI/Program.cs
+++ Panda.Portfolio.WebUI/Program.cs
@@ -1,9 +1,3 @@
-using System.Net;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
-
-using Microsoft.AspNetCore.DataProtection;
-
 using MudBlazor.Services;
 
 using Panda.BlazorCore.Services;
@@ -55,8 +49,15 @@ void ConfigureRequests()
 void ConfigureServices()
 {
     var Services = builder.Services;
+
+#if !DEBUG
     Services.AddDataProtection()
-        .ProtectKeysWithCertificate(buildSelfSignedServerCertificate());
+        .SetApplicationName("p8b-portfolio")
+        .PersistKeysToFileSystem(new DirectoryInfo("../../PersistKeys"))
+        .ProtectKeysWithCertificate(
+    new X509Certificate2("certificate.pfx", builder.Configuration["CertificatePassword"]));
+#endif
+
     Services.AddMudServices();
 
     Services.AddRazorPages(options => options.RootDirectory = "/App");
@@ -64,32 +65,4 @@ void ConfigureServices()
     Services.AddServerSideBlazor();
 
     Services.AddTransient<IJSService, JSService>();
-}
-X509Certificate2 buildSelfSignedServerCertificate()
-{
-    const string CertificateName = "p8b.uk";
-    SubjectAlternativeNameBuilder sanBuilder = new();
-    sanBuilder.AddIpAddress(IPAddress.Loopback);
-    sanBuilder.AddIpAddress(IPAddress.IPv6Loopback);
-    sanBuilder.AddDnsName("test.p8b.uk");
-    sanBuilder.AddDnsName(Environment.MachineName);
-
-    X500DistinguishedName distinguishedName = new($"CN={CertificateName}");
-
-    using RSA rsa = RSA.Create(2048);
-    var request = new CertificateRequest(distinguishedName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-
-    request.CertificateExtensions.Add(
-        new X509KeyUsageExtension(X509KeyUsageFlags.DataEncipherment | X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DigitalSignature, false));
-
-    request.CertificateExtensions.Add(
-       new X509EnhancedKeyUsageExtension(
-           new OidCollection { new Oid("1.3.6.1.5.5.7.3.1") }, false));
-
-    request.CertificateExtensions.Add(sanBuilder.Build());
-
-    var certificate = request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), new DateTimeOffset(DateTime.UtcNow.AddDays(3650)));
-    certificate.FriendlyName = CertificateName;
-
-    return new X509Certificate2(certificate.Export(X509ContentType.Pfx, "WeNeedASaf3rPassword"), "WeNeedASaf3rPassword", X509KeyStorageFlags.MachineKeySet);
 }
